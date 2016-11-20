c68d906b6943d2ec8e6562e04976ea4fcc1c0f71
Rozita Teymourzadeh <Rozita.Teymourzadeh@neonode.com>
2016-11-20 12:36:18 +0100

    url_creator file

---
+++ url_creator.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 13:20:12 2016
+    
+@author: rozita.teymourzadeh
+"""
+
+config = {'base_url':'http://www.rozitateymourzadeh.com','pages':['home','about','contact']}
+   
+def base_url():
+	return config['base_url']
+	
+def get_page_url(name):
+	page_name = ''
+	if name in config['pages']:
+		page_name = name
+		
+	return "%s/%s" % (base_url(),page_name)
+	
+if __name__ == "__main__":
+	print get_page_url('home')
+	print get_page_url('services')
+	print get_page_url('contact')
+       

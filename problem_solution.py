6718ee12b9c98febbd1e8eb8d6644af532e9d04b
RozitaTeymourzadeh <rozita.tei@gmail.com>
2021-08-26 15:40:41 -0700

    Q5

--- problem_solution.py
+++ problem_solution.py
@@ -57,6 +57,32 @@ class Solution:
                     median /= float(2)
             return median
 
+        """
+        5. Given a string s, return the longest palindromic substring in s.
+        Input: s = "babad"
+        Output: "bab"
+        Note: "aba" is also a valid answer.
+
+        """
+
+        def longestPalindrome(self, s: str) -> str:
+            self.maxlen = 0
+            self.start = 0
+
+            for i in range(len(s)):
+                self.expandFromCenter(s, i, i)
+                self.expandFromCenter(s, i, i + 1)
+            return s[self.start:self.start + self.maxlen]
+
+        def expandFromCenter(self, s, l, r):
+            while l > -1 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+
+            if self.maxlen < r - l - 1:
+                self.maxlen = r - l - 1
+                self.start = l + 1
+
 if __name__ == "__main__":
     obj = Solution()
 

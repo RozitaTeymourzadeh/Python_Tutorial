88f55a45f456440b32c6c4e10ce1efd5534cd0ad
RozitaTeymourzadeh <rozita.tei@gmail.com>
2021-09-20 12:11:48 -0700

    Q10

--- problem_solution.py
+++ problem_solution.py
@@ -230,10 +230,62 @@ def isPalindrome(x: int) ->bool:
         return True
     return False
 
+"""
+10. Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
 
+'.' Matches any single character
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
 
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+Input: s = "aab", p = "c*a*b"
+Output: true
+Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+
+Input: s = "mississippi", p = "mis*is*p*."
+Output: false
+
+Condition:
+1 <= s.length <= 20
+1 <= p.length <= 30
+s contains only lowercase English letters.
+p contains only lowercase English letters, '.', and '*'.
+It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+
+"""
+def isMatch(self, s: str, p: str) -> bool:
+    # TOP-Down Memoization
+    # Ref: NeetCode
+    cache = {}
+    def dfs(i,j):
+        if (i, j) in cache:
+            return cache[(i, j)]
+        if i >= len(s) and j >= len(p):
+            return True
+        if j >= len(p):
+            return False
+
+        match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+        if (j + 1) < len(p) and p[j + 1] == "*":
+            cache[(i, j)] = (dfs(i,j+2) or      #dont use *
+                    (match and dfs(i+1, j)))    # use *
+            return cache[(i, j)]
+        if match:
+            cache[(i, j)] = dfs(i+1, j+1)
+            return cache[(i, j)]
+        cache[(i, j)] = False
+        return False
+
+    return dfs(0, 0)
 
 if __name__ == "__main__":
     print("Hello")
-    result = isPalindrome( x=121)
+    result = isPalindrome(x=121)
     print(result)

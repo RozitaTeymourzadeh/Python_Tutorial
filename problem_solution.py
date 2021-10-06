a1a3f37f885fb594f7d45e6e8b75f49b83caac7f
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-10-06 12:19:59 -0700

    Q20

--- problem_solution.py
+++ problem_solution.py
@@ -692,7 +692,6 @@ The number of nodes in the list is sz.
 class ListNode(object):
     pass
 
-
 def removeNthFromEnd(self, head: ListNode, n:int)-> ListNode:
     dummy = ListNode(0, head)
     left = dummy
@@ -708,6 +707,61 @@ def removeNthFromEnd(self, head: ListNode, n:int)-> ListNode:
 
     left.next = left.next.next
     return dummy.next
+
+'''
+Q20 Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+Example 4:
+
+Input: s = "([)]"
+Output: false
+Example 5:
+
+Input: s = "{[]}"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+'''
+def isValid(self, s: str) -> bool:
+    stack = []
+    closeToOpen = {")":"(","}": "{", "]":"["}
+
+    for c in s:
+        if c in closeToOpen:
+            if stack and stack[-1] == closeToOpen[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+    return True if not stack else False
+
+
+
 
 if __name__ == "__main__":
     print("Hello")

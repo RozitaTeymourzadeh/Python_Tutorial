b508f8aeec4966ead5b42ecaa52846bbc043d837
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-10-07 22:11:18 -0700

    Q21

--- problem_solution.py
+++ problem_solution.py
@@ -760,7 +760,52 @@ def isValid(self, s: str) -> bool:
             stack.append(c)
     return True if not stack else False
 
+'''
+Q21 Merge Two SortedList
+Merge two sorted linked lists and return it as a sorted list. 
+The list should be made by splicing together the nodes of the first two lists.
 
+ 
+
+Example 1:
+
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: l1 = [], l2 = []
+Output: []
+Example 3:
+
+Input: l1 = [], l2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both l1 and l2 are sorted in non-decreasing order.
+'''
+def mergeTwoLists(self, l1: ListNode , l2: ListNode) -> ListNode:
+    dummy = ListNode()
+    tail = dummy
+
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+
+    if l1:
+        tail.next = l1
+    elif l2:
+        tail.next = l2
+    return dummy.next
 
 
 if __name__ == "__main__":

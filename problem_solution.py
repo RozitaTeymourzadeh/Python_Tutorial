46dfb521c1694b29ced98e3c1ad15bd51e024966
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-10-08 16:20:07 -0700

    Q23

--- problem_solution.py
+++ problem_solution.py
@@ -855,7 +855,58 @@ def generateParenthesis(self, n: int) -> List[str]:
     return res
 
 
+'''
+Q23: Merge k sorted Lists
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length won't exceed 10^4.
+'''
+def mergeKLists(self, lists:List[ListNode])-> ListNode:
+    if not lists or len(lists) == 0:
+        return None
+
+    while len(lists) > 1:
+        mergedList = []
+
+        for i in range (0, len(lists), 2):
+            l1 = lists[i]
+            l2 = lists[i+1] if i+1 < len(lists) else None
+            mergedList.append(self.mergeList(l1,l2))
+        lists = mergedList
+    return lists[0]
 
 
 if __name__ == "__main__":

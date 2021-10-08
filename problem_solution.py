3293fe8b17221fe5363545ad76a727616d10fc9c
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-10-08 13:13:15 -0700

    Q22

--- problem_solution.py
+++ problem_solution.py
@@ -807,6 +807,56 @@ def mergeTwoLists(self, l1: ListNode , l2: ListNode) -> ListNode:
         tail.next = l2
     return dummy.next
 
+"""
+Q22. Generate Parentheses
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+ 
+
+Constraints:
+
+1 <= n <= 8
+
+"""
+def generateParenthesis(self, n: int) -> List[str]:
+
+    # only add open paranthesis if open < n
+    # only add a closing paranthesis if closed < open
+    # valid if open == closed == n
+
+    stack = []
+    res = []
+
+    def backtrack(openN, closedN):
+        if openN == closedN == n:
+            res.append("".join(stack))
+            return
+
+        if openN < n:
+            stack.append("(")
+            backtrack(openN+1, closedN)
+            stack.pop()
+
+        if closedN <openN:
+            stack.append(")")
+            backtrack(openN, closedN +1)
+            stack.pop()
+    backtrack(0,0)
+    return res
+
+
+
+
 
 if __name__ == "__main__":
     print("Hello")

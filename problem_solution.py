a5fd1a35bc70700a72c0acaf429625ad7c6ff69b
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-10-08 17:59:53 -0700

    Q24

--- problem_solution.py
+++ problem_solution.py
@@ -908,6 +908,48 @@ def mergeKLists(self, lists:List[ListNode])-> ListNode:
         lists = mergedList
     return lists[0]
 
+"""
+24. Swap Nodes in Pairs
+
+Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+Example 2:
+Input: head = []
+Output: []
+
+Example 3:
+Input: head = [1]
+Output: [1]
+ 
+Constraints:
+The number of nodes in the list is in the range [0, 100].
+0 <= Node.val <= 100
+"""
+def swapPairs(self, head: ListNode) -> ListNode:
+
+    dummy = ListNode(0, head)
+    prev, curr = dummy, head
+
+    while curr and curr.next:
+        # save node
+        nextPair = curr.next.next
+        second = curr.next
+
+        # reverse this pair
+        curr.next = nextPair
+        second.next = curr
+        prev.next = second
+
+        #update pointer
+        prev = curr
+        curr = nextPair
+    return dummy.next
+
+
 
 if __name__ == "__main__":
     print("Hello")

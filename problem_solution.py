9a0d5d6d8a1b9e7f0d500ff48eaf0183bc2f050e
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-10-11 13:49:16 -0700

    Q25

--- problem_solution.py
+++ problem_solution.py
@@ -950,6 +950,55 @@ def swapPairs(self, head: ListNode) -> ListNode:
     return dummy.next
 
 
+"""
+Q25. Reverse Nodes in K-Group 
+
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+
+Example 2
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+"""
+def reverseKGroup(self, head:ListNode, k: int)-> ListNode:
+    dummy = ListNode(0, head)
+    groupPrev = dummy
+
+    while True:
+        kth = self.getKth(groupPrev, k)
+        if not kth:
+            break
+        groupNext = kth.next
+
+        # reverse group
+        prev, curr = kth.next, groupPrev.next
+        while curr != groupNext:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp # update node
+        tmp = groupPrev.next
+        groupPrev.next = kth
+        groupPrev = tmp
+    return dummy.next
+
+    def getKth(self, curr, k):
+        while curr and k >0:
+            curr = curr.next
+            k -= 1
+        return curr
+
+
+
+
 
 if __name__ == "__main__":
     print("Hello")

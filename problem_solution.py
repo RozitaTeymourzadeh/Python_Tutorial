7492eca6129fe6ee468dcdbaf2024955131af45e
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-10-12 10:21:14 -0700

    Q29

--- problem_solution.py
+++ problem_solution.py
@@ -1148,6 +1148,61 @@ def strStr(self, haystack:str, needle: str) ->int:
             return i
     return -1
 
+"""
+Q29. Devide Two Integers
+
+Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+
+Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, assume that your function returns 231 − 1 when the division result overflows.
+
+Example 1:
+
+Input: dividend = 10, divisor = 3
+Output: 3
+Explanation: 10/3 = truncate(3.33333..) = 3.
+Example 2:
+
+Input: dividend = 7, divisor = -3
+Output: -2
+Explanation: 7/-3 = truncate(-2.33333..) = -2.
+Example 3:
+
+Input: dividend = 0, divisor = 1
+Output: 0
+Example 4:
+
+Input: dividend = 1, divisor = 1
+Output: 1
+ 
+
+Constraints:
+
+-231 <= dividend, divisor <= 231 - 1
+divisor != 0
+
+"""
+def divide(self, dividend: int , divisor: int) -> int:
+    d = abs(dividend)
+    dv = abs(divisor)
+
+    output = 0
+
+    while d >= dv:
+        tmp = dv
+        mul = 1
+        while d >= tmp: # This approach is to have log n time complexity, we exponentially subtract tmp from d
+            d -= tmp
+            output += mul
+            mul += mul
+            tmp += tmp
+        if (dividend < 0 and divisor >=0) or (divisor <0 and dividend >= 0):
+            output = - output
+        return min(2147483647, max(-2147483648, output))
+
 
 if __name__ == "__main__":
     print("Hello")

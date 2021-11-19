1195b29f34a6ffbcc45054b37ba53aee11149427
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-11-19 13:01:03 -0800

    Q37

--- problem_solution.py
+++ problem_solution.py
@@ -1591,6 +1591,67 @@ def isValidSudoku(self, board: List[List[str]]) -> bool:
             squares[(r // 3, c // 3)].add(board[r][c])
     return True
 
+"""
+Q37) Sudoku Solver
+
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+ 
+
+Example 1:
+
+
+Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+It is guaranteed that the input board has only one solution.
+
+"""
+def solveSudoku(self, board: List[List[str]]) -> None:
+    def isValid(bard, r, c, num_str):
+        for j in range(9):
+            if board[r][j] == num_str:
+                return False
+        for i in range(9):
+            if board[i][c] == num_str:
+                return False
+        x = r // 3
+        y = c // 3
+        for i in range(3 * x, 3 * x + 3):
+            for j in range(3 * y, 3 * y + 3):
+                if board[i][j] == num_str:
+                    return False
+        return True
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == ".":
+                for num_str in "123456789":
+                    if isValid(board, i, j, num_str):
+                        board[i][j] = num_str
+                        if self.solveSudoku(board):
+                            return True
+                        else:
+                            board[i][j] = "."
+                return False
+    return True
+
 
 if __name__ == "__main__":
     print("Hello")

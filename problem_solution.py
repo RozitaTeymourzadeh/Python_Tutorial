b093303e88fd1618058dd551b5ea46a53c0d5eeb
RozitaTeymourzadeh <rozita.teim@gmail.com>
2021-11-18 16:06:33 -0800

    Q34-Q35

--- problem_solution.py
+++ problem_solution.py
@@ -1415,11 +1415,118 @@ class Solution:
                     l = mid + 1
         return -1
 
+'''
+34. Find First and Last Position of Element in Sorted Array
+
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+
+'''
+def searchRange(self, nums: List[int], target: int) -> List[int]:
+    left = self.binSearch(nums, target, True)
+    right = self.binSearch(nums, target, False)
+
+    return [left, right]
+
+
+# leftBias=[True/False], if false, res is rightBiased
+def binSearch(self, nums, target, leftBias):
+    l, r = 0, len(nums) - 1
+    i = -1
+    while l <= r:
+        m = (l + r) // 2
+        if target > nums[m]:
+            l = m + 1
+        elif target < nums[m]:
+            r = m - 1
+        else:
+            i = m
+            if leftBias:
+                r = m - 1
+            else:
+                l = m + 1
+    return i
+
+'''
+35. Search Insert Position
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+Example 4:
+
+Input: nums = [1,3,5,6], target = 0
+Output: 0
+Example 5:
+
+Input: nums = [1], target = 0
+Output: 0
+ 
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+
+'''
+def searchInsert(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if target == nums[m]:
+            return m
+        elif target < nums[m]:
+            r = m - 1
+        else:
+            l = m + 1
+    return l
+
 
 if __name__ == "__main__":
     print("Hello")
-    result = isPalindrome(x=121)
-    max_volume = maxArea(height=[1,8,6,2,5,4,8,3,7])
-    result_3sum = threeSumClosest([-1,2,1,-4])
+    # result = isPalindrome(x=121)
+    # max_volume = maxArea(height=[1,8,6,2,5,4,8,3,7])
+    # result_3sum = threeSumClosest([-1,2,1,-4])
+    print(searchInsert([1, 3, 5, 6], 7))
+
+
+
     # print(solution().letterCombination("34"))
-    print(result)
+    # print(result)
